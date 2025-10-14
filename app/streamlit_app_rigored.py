5b89f213815598bdca69314158b3fa0e34ba5c12
invertedstructure <ccmenaia@gmail.com>
2025-10-14 08:33:53 +0100

    Update streamlit_app_rigored.py

--- app/streamlit_app_rigored.py
+++ app/streamlit_app_rigored.py
@@ -6041,8 +6041,7 @@ with safe_expander("Cert & provenance", expanded=True):
              st.success("Inputs OK")
              _chip("b", inputs_sig[0]); _chip("C", inputs_sig[1])
              _chip("H", inputs_sig[2]); _chip("U", inputs_sig[3]); _chip("S", inputs_sig[4])
-        
-            show_hashes = st.checkbox("Show copyable hashes", value=False, key="copy_hashes_toggle")
+                    show_hashes = st.checkbox("Show copyable hashes", value=False, key="copy_hashes_toggle")
            if show_hashes:
               _copybox(",".join(inputs_sig), key="copy_inputs_sig")
             else:
